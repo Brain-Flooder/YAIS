469127d3cb6d32b1b031033d81d9d4333b8c8a78
Brain-Flooder <baobui.a1@gmail.com>
2021-10-12 11:06:37 +0000

    e

--- cogs/giveaway.py
+++ cogs/giveaway.py
@@ -12,16 +12,24 @@ class GiveawayCommands(commands.Cog, name='FunCommands'):
     @commands.has_permissions(manage_guild=True)
     async def cgw(self,ctx,times:int,winners,*,prize):
       eh = time.time()
+      x=0
       for x in range(0,times):
-        eh+=60
+        eh+=1
+        x+=1
       eh=int(eh)
+      print(x)
       embed=discord.Embed()
       embed.add_field(name=prize,value=f'React with 🎉 to enter!\nTime: <t:{eh}:R>\nHosted by: {ctx.author.mention}')
       embed.set_footer(text = f'{winners} winner(s)')
       gwlink = await ctx.send(embed=embed)
       await gwlink.add_reaction('🎉')
-      await asyncio.sleep(eh)
-      await ctx.send(f"{gwlink.reactions}")
-
+      await asyncio.sleep(x)
+      for s in gwlink.reactions:
+        if s.emoji.name == "tada":
+          users = await s.users().flatten()
+          winner = random.choice(users)
+          await gwlink.channel.send(f'{winner.mention} has won the raffle.')
+          
+          
 def setup(bot):
-    bot.add_cog(GiveawayCommands(bot))
+  bot.add_cog(GiveawayCommands(bot))
